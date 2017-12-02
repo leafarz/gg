3f2f2a88b2b3a91a968d152e111881f8960cd439
Raf Formes <rafformes@gmail.com>
2017-12-02 20:10:07 +0800

    added gitignore, added dependency libraries, added engine project, added sandbox project, added project dependencies, added header, library and binary dependencies to project settings, added post build events, set default custom directories, added sample scripts

---
+++ ggEngine/src/core/CoreEngine.cpp
@@ -0,0 +1,13 @@
+#include "core/CoreEngine.h"
+
+namespace GGEngine
+{
+	CoreEngine::CoreEngine(void)
+	{
+	}
+
+	CoreEngine::~CoreEngine(void)
+	{
+	}
+
+}// namespace ggEngine
---
+++ sandBox/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "core/CoreEngine.h"
+
+int main()
+{
+	GGEngine::CoreEngine _coreEngine = GGEngine::CoreEngine();
+
+	system("PAUSE");
+	return -1;
+}
