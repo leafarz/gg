f4f736d0ac56c43c28546354fd5bcd2adf62e2e5
Raf Formes <rafformes@gmail.com>
2018-02-05 19:44:04 +0800

    added container functions for camera

--- gg/include/component/Camera.h
+++ gg/include/component/Camera.h
@@ -4,18 +4,28 @@
 
 #include "component/Component.h"
 
+#include "math/Mat4f.h"
+#include "core/Types.h"
 namespace gg
 {
 	class Camera : public Component
 	{
 	public:
-		Camera(void);
+		Camera(float fovDeg, float aspectRatio, float zNear, float zFar);
 		~Camera(void);
 
 		static ComponentType getStaticType(void) { return ComponentType::Camera; }
 		virtual ComponentType getType(void) const override { return getStaticType(); }
 
+		void setPerspectiveCamera(float fovDeg, float aspectRatio, float zNear, float zFar);
+
+		const Math::Mat4f& getProjectionMatrix(void) const;
+		const Math::Mat4f& getViewProjectionMatrix(void);
+
 		void update(void);
+
+	private:
+		Math::Mat4f m_ProjectionMatrix;
 
 	}; // class Camera
 } // namespace gg
--- gg/src/component/Camera.cpp
+++ gg/src/component/Camera.cpp
@@ -1,12 +1,30 @@
 #include "component/Camera.h"
 
+#include "entity/GameObject.h"
 namespace gg
 {
-	Camera::Camera(void)
+	Camera::Camera(float fovDeg, float aspectRatio, float zNear, float zFar)
+		: m_ProjectionMatrix(Math::Mat4f::perspectiveMatrix(fovDeg, aspectRatio, zNear, zFar))
 	{ }
 
 	Camera::~Camera(void)
 	{ }
+
+	void Camera::setPerspectiveCamera(float fovDeg, float aspectRatio, float zNear, float zFar)
+	{
+		m_ProjectionMatrix = Math::Mat4f::perspectiveMatrix(fovDeg, aspectRatio, zNear, zFar);
+	}
+
+	const Math::Mat4f & Camera::getProjectionMatrix(void) const
+	{
+		return m_ProjectionMatrix;
+	}
+
+	const Math::Mat4f & Camera::getViewProjectionMatrix(void)
+	{
+		Transform* _t = m_GameObject->getTransform();
+		// TODO: insert return statement here
+	}
 
 	void Camera::update(void)
 	{ }
